5b2962b08b903eabd2b488f414112486bb332f17
salsabillaazzahra172-hash <salsabillaazzahra172@gmail.com>
2025-12-31 12:06:28 +0700

    menambahkan user

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test', function () {
+Route::get('/test', function () { 
     return view('test');
+});
+
+Route::get('/user', function () {
+    return view('user');
 });
